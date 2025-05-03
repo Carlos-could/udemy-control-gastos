213e3d1f9c46d6956da16ba1cb26a2aad3cebff6
Carlos-could <jjbbpe@gmail.com>
2025-05-03 08:07:51 +0200

    202

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import  {useMemo} from 'react'
+import { useBudget } from "./hooks/useBudget"
+
 import BudgetForm from "./components/BudgetForm"
+import BudgetTracker from "./components/BudgetTracker"
+
 
 function App() {
 
+  const { state } = useBudget()
+  const isValidBudget = useMemo(() => state.budget > 0, [state.budget])
 
   return (
     <>
@@ -9,7 +16,8 @@ function App() {
         <h1 className="text-center text-4xl text-white">Planificador de gastos</h1>
       </header>
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-        <BudgetForm/>
+        {isValidBudget ? <BudgetTracker/>: <BudgetForm/>}
+        
       </div>
     </>
   )
